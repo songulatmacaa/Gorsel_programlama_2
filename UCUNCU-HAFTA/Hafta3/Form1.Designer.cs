831490a5b293a95aced01b32c4b2b9940667db8c
Songül Atmaca <144557129+songulatmacaa@users.noreply.github.com>
2025-10-09 14:39:05 +0300

    BUTONCARPISMASI

---
+++ UCUNCU-HAFTA/Hafta3/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Hafta3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(70, 70);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(407, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(70, 70);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 384);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(70, 70);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(407, 384);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(70, 70);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(498, 474);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Hafta 3";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ UCUNCU-HAFTA/Hafta3/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hafta3
+{
+    public partial class Form1 : Form
+    {
+        private Timer animationTimer;
+        private Point button1Start, button2Start, button3Start, button4Start;
+        private Point button1Target, button2Target, button3Target, button4Target;
+        private bool movingToCenter = true;
+        private int step = 0;
+        private int totalSteps = 50; // Animasyon adım sayısı
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeAnimation();
+        }
+
+        private void InitializeAnimation()
+        {
+            // Başlangıç pozisyonlarını kaydet
+            button1Start = button1.Location;
+            button2Start = button2.Location;
+            button3Start = button3.Location;
+            button4Start = button4.Location;
+
+            // Butonlar arasındaki boşluk
+            int spacing = 5;
+
+            // Karenin toplam genişliği ve yüksekliği
+            int totalWidth = button1.Width * 2 + spacing * 2;
+            int totalHeight = button1.Height * 2 + spacing * 2;
+
+            // Karenin sol üst köşesinin merkeze göre konumu
+            int squareLeft = (this.ClientSize.Width - totalWidth) / 2;
+            int squareTop = (this.ClientSize.Height - totalHeight) / 2;
+
+            // Sol üst buton
+            button1Target = new Point(squareLeft, squareTop);
+
+            // Sağ üst buton
+            button2Target = new Point(
+                squareLeft + button1.Width + spacing * 2,
+                squareTop
+            );
+
+            // Sol alt buton
+            button3Target = new Point(
+                squareLeft,
+                squareTop + button1.Height + spacing * 2
+            );
+
+            // Sağ alt buton
+            button4Target = new Point(
+                squareLeft + button1.Width + spacing * 2,
+                squareTop + button1.Height + spacing * 2
+            );
+
+            // Timer'ı oluştur
+            animationTimer = new Timer();
+            animationTimer.Interval = 20; // 20ms = yaklaşık 50 FPS
+            animationTimer.Tick += AnimationTimer_Tick;
+            animationTimer.Start();
+        }
+
+        private void AnimationTimer_Tick(object sender, EventArgs e)
+        {
+            step++;
+
+            if (step <= totalSteps)
+            {
+                // Animasyon devam ediyor
+                float progress = (float)step / totalSteps;
+
+                if (movingToCenter)
+                {
+                    // Merkeze doğru hareket
+                    button1.Location = Lerp(button1Start, button1Target, progress);
+                    button2.Location = Lerp(button2Start, button2Target, progress);
+                    button3.Location = Lerp(button3Start, button3Target, progress);
+                    button4.Location = Lerp(button4Start, button4Target, progress);
+                }
+                else
+                {
+                    // Başlangıç pozisyonlarına dön
+                    button1.Location = Lerp(button1Target, button1Start, progress);
+                    button2.Location = Lerp(button2Target, button2Start, progress);
+                    button3.Location = Lerp(button3Target, button3Start, progress);
+                    button4.Location = Lerp(button4Target, button4Start, progress);
+                }
+            }
+            else
+            {
+                // Animasyon tamamlandı
+                step = 0;
+
+                if (movingToCenter)
+                {
+                    // Merkeze ulaştık, biraz bekle
+                    animationTimer.Stop();
+                    Timer delayTimer = new Timer();
+                    delayTimer.Interval = 500;
+                    delayTimer.Tick += (s, ev) =>
+                    {
+                        delayTimer.Stop();
+                        delayTimer.Dispose();
+                        movingToCenter = false;
+                        animationTimer.Start();
+                    };
+                    delayTimer.Start();
+                }
+                else
+                {
+                    // Başlangıca döndük, biraz bekle ve tekrar başla
+                    animationTimer.Stop();
+                    Timer delayTimer = new Timer();
+                    delayTimer.Interval = 500;
+                    delayTimer.Tick += (s, ev) =>
+                    {
+                        delayTimer.Stop();
+                        delayTimer.Dispose();
+                        movingToCenter = true;
+                        animationTimer.Start();
+                    };
+                    delayTimer.Start();
+                }
+            }
+        }
+
+        // Linear interpolation (doğrusal enterpolasyon)
+        private Point Lerp(Point start, Point end, float t)
+        {
+            return new Point(
+                (int)(start.X + (end.X - start.X) * t),
+                (int)(start.Y + (end.Y - start.Y) * t)
+            );
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            if (animationTimer != null)
+            {
+                animationTimer.Stop();
+                animationTimer.Dispose();
+            }
+            base.OnFormClosing(e);
+        }
+    }
+}
